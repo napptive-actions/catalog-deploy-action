bf9d7ef243e537b7c3af64962bdaf8884caabc11
Carmendelope <carmen@napptive.com>
2022-02-22 18:36:26 +0100

    playgound cmd in /app dir

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,9 +10,6 @@ appName=$1
 environment=$2
 configFile=$3
 
-# add actual dir to the path to allow execute the playground command
-export PATH=$PATH:./
-
 ## appName cannot be empty
 if [[ -z "$appName" ]]; then
     echo "appName can not be empty"
@@ -27,7 +24,7 @@ fi
 
 # Step 1. Login in to the platform
 # Login into the platform (with pat flag)
-playground login --pat
+/app/playground login --pat
 if [[ $? -ne 0 ]]; then
     exit -1
 fi
@@ -35,14 +32,14 @@ fi
 
 # if environment!= "" -> use it!
 if [[ -n "$environment" ]]; then
-    playground env use ${environment}
+    /app/playground env use ${environment}
     if [[ $? -ne 0 ]]; then    
         exit -1
     fi
 fi
 echo "deploying ${appName}..."
 # deploy the app
-playground catalog deploy ${appName}
+/app/playground catalog deploy ${appName}
 if [[ $? -ne 0 ]]; then    
     exit -1
 fi
